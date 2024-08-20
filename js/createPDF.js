f1503820544844e46d52a027e973843e5b8879b8
mvmeel05 <m.van.meel@beesda2.nl>
2024-08-20 15:02:23 +0200

    Handtekening en merge toegevoegd

--- js/createPDF.js
+++ js/createPDF.js
@@ -11,11 +11,31 @@ async function  aanmakenPDF (setletter, pdfGUID){
     const respPDFfile = await getPDFfile(setletter, pdfGUID);
     let   jsonPDFfile = await respPDFfile;
    
-  // console.log('setletter: '+ setletter);	
-  // console.log('pdfJSON: ' + jsonPDFfile[0].PDFFILE_JSON.trim());
-  // console.log('pdfGUID: ' + jsonPDFfile[0].PDFFILE_GUID.trim());
+    console.log('pdffile_kenmerk: ' + jsonPDFfile[0].PDFFILE_KENMERK);
+     
+    if ((jsonPDFfile[0].PDFFILE_KENMERK).trim() === 'CHECKLIST')
+  {		 
+ // samestellen PDF document tbv Checklist
+   console.log("samenstellen PDF CHECKLIST");
+   const respSamenstellenPDF_Checklist = await samenstellenPDF_Checklist (setletter, jsonPDFfile[0].PDFFILE_FILIAAL, jsonPDFfile[0].PDFFILE_DOCUMENT, jsonPDFfile[0].PDFFILE_OORSPRONG, jsonPDFfile[0].PDFFILE_CHKLSTNR, jsonPDFfile[0].PDFFILE_JSON);
+   let   response  = await respSamenstellenPDF_Checklist;
+  }
   
-   
+   if ((jsonPDFfile[0].PDFFILE_KENMERK).trim() === 'HANDTEKENING')
+  {		 
+ // samestellen PDF document tbv Handtekening
+   console.log("samenstellen PDF HANDTEKENING");
+   const respSamenstellenPDF_Handtekening = await samenstellenPDF_Handtekening (setletter, jsonPDFfile[0].PDFFILE_FILIAAL, jsonPDFfile[0].PDFFILE_DOCUMENT, jsonPDFfile[0].PDFFILE_OORSPRONG, jsonPDFfile[0].PDFFILE_JSON);
+   let   response  = await respSamenstellenPDF_Handtekening;
+  }
+  
+   if ((jsonPDFfile[0].PDFFILE_KENMERK).trim() === 'MERGE_PDF_DOCUMENTEN')
+  {		 
+ // samestellen PDF document tbv Handtekening
+   console.log("merge 1 document");
+   const respMergPDF_Document = await mergePDFdocumenten (setletter, jsonPDFfile[0].PDFFILE_FILIAAL, jsonPDFfile[0].PDFFILE_DOCUMENT, jsonPDFfile[0].PDFFILE_OORSPRONG, jsonPDFfile[0].PDFFILE_JSON);
+   let   response  = await respMergPDF_Document;
+  }
     
 
   // samestellen PDF document
